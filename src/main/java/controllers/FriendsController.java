92d51ccb007ba8ca0898fc3f46512127cbd0f740
VivaNova99 <veraivanova9999@gmail.com>
2017-04-14 22:12:06 +0300

    FriendsController

--- src/main/java/controllers/FriendsController.java
+++ src/main/java/controllers/FriendsController.java
@@ -15,33 +15,32 @@ import static model.User.FIRST_NAME_KEY;
 import static model.User.ID_KEY;
 
 
-@WebServlet("/reg-user-news")
-public class NewsController extends HttpServlet {
+@WebServlet("/reg-user-all-friends")
+public class FriendsController extends HttpServlet {
 
     public static final String WELCOME_KEY = "Welcome";
     public static final String ALL_USERS_KEY = "AllUser";
-    public static final String ALL_FORUM_THEMES_KEY = "AllForumThemes";
+//    public static final String ALL_FORUM_THEMES_KEY = "AllForumThemes";
 //    public static final String ALL_PHOTO_ALBUMS_KEY = "AllPhotoAlbums";
     public static final String ALL_PHOTOS_KEY = "AllPhotos";
 //    public static final String ALL_PRIVATE_MESSAGES_KEY = "AllPrivateMessages";
-    public static final String ALL_WALL_MESSAGES_KEY = "AllWallMessages";
-    public static final String LAST_10_WALL_MESSAGES_KEY = "Last10WallMessages";
+//    public static final String ALL_WALL_MESSAGES_KEY = "AllWallMessages";
 
     private UserDao userDao;
-    private ForumThemeDao forumThemeDao;
+//    private ForumThemeDao forumThemeDao;
 //    private PhotoAlbumDao photoAlbumDao;
     private PhotoDao photoDao;
 //    private PrivateMessageDao privateMessageDao;
-    private WallMessageDao wallMessageDao;
+//    private WallMessageDao wallMessageDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         userDao = (UserDao) config.getServletContext().getAttribute("UserDao");
-        forumThemeDao = (ForumThemeDao) config.getServletContext().getAttribute("ForumThemeDao");
+//        forumThemeDao = (ForumThemeDao) config.getServletContext().getAttribute("ForumThemeDao");
 //        photoAlbumDao = (PhotoAlbumDao) config.getServletContext().getAttribute("PhotoAlbumDao");
         photoDao = (PhotoDao) config.getServletContext().getAttribute("PhotoDao");
 //        privateMessageDao = (PrivateMessageDao) config.getServletContext().getAttribute("PrivateMessageDao");
-        wallMessageDao = (WallMessageDao) config.getServletContext().getAttribute("WallMessageDao");
+//        wallMessageDao = (WallMessageDao) config.getServletContext().getAttribute("WallMessageDao");
     }
 
 
@@ -49,16 +48,15 @@ public class NewsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("!!!!!"+this);
         String s = Optional.ofNullable(req.getSession().getAttribute(FIRST_NAME_KEY))
                 .map(o -> String.format("Здравствуйте, %s", o))
                 .orElse("Здравствуйте!");
 
 //        String userPageOrNot = Optional.ofNullable(req.getSession().getAttribute(String.valueOf(ID_KEY)))
-//                .map(o -> String.format("/WEB-INF/reg-user-own-page/%s.jsp", o)).
-////                orElse("/WEB-INF/unreg-forum.jsp");
-////                orElse("/WEB-INF/test.jsp");
-////                orElse("/WEB-INF/reg-user-own-page.jsp");
-//                orElse("/WEB-INF/index.jsp");
+//                .map(o -> String.format("reg-user-own-page/%s.jsp", o)).
+//                orElse("test.jsp");
+////                orElse("/WEB-INF/index.jsp");
 
 //        boolean b = Optional.ofNullable(req.getSession().getAttribute(String.valueOf(ID_KEY)))
 //                .map(o -> true)
@@ -67,14 +65,11 @@ public class NewsController extends HttpServlet {
         req.setAttribute(WELCOME_KEY, s);
 
         req.setAttribute(ALL_USERS_KEY, userDao.getAll());
-        req.setAttribute(ALL_FORUM_THEMES_KEY, forumThemeDao.getAll());
+//        req.setAttribute(ALL_FORUM_THEMES_KEY, forumThemeDao.getAll());
 //        req.setAttribute(ALL_PHOTO_ALBUMS_KEY, photoAlbumDao.getAll());
         req.setAttribute(ALL_PHOTOS_KEY, photoDao.getAll());
 //        req.setAttribute(ALL_PRIVATE_MESSAGES_KEY, privateMessageDao.getAll());
-        req.setAttribute(ALL_WALL_MESSAGES_KEY, wallMessageDao.getAll());
-        req.setAttribute(LAST_10_WALL_MESSAGES_KEY, wallMessageDao.getLast10());
-
-
+//        req.setAttribute(ALL_WALL_MESSAGES_KEY, wallMessageDao.getAll());
 
 //        if (b) {req.getRequestDispatcher("/WEB-INF/reg-user-own-page.jsp")
 //                .forward(req, resp);
@@ -85,7 +80,7 @@ public class NewsController extends HttpServlet {
 
 //        req.getRequestDispatcher(userPageOrNot).forward(req, resp);
 
-        req.getRequestDispatcher("reg-user-news.jsp")
+        req.getRequestDispatcher("reg-user-all-friends.jsp")
                 .forward(req, resp);
     }
 }
--- src/main/webapp/reg-user-all-friends.jsp
+++ src/main/webapp/reg-user-all-friends.jsp
@@ -1,4 +1,6 @@
-<%--
+<%@ page import="controllers.FriendsController" %>
+<%@ page import="model.*" %>
+<%@ page import="java.util.Collection" %><%--
   Created by IntelliJ IDEA.
   User: veraivanova
   Date: 11.04.17
@@ -8,7 +10,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+    <title>${requestScope.get(WelcomeController.WELCOME_KEY)} Добро пожаловать</title>
 </head>
 <body>
 
@@ -19,6 +21,47 @@
 <aside class="reg-user">
     <%@ include file="WEB-INF/sidebar-reg-user.jsp" %>
 </aside>
+
+<section>
+
+    <%
+        Collection<User> users = (Collection<User>) request.getAttribute(FriendsController.ALL_USERS_KEY);
+    %>
+
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Date of birth</th>
+            <th>Access Level</th>
+            <th>Email</th>
+            <th>Password hash</th>
+            <th>Profile photo</th>
+            <th>Status on wall</th>
+            <th>City</th>
+        </tr>
+            <%
+        for (User user: users) {
+    %>
+        <tr>
+            <td><%=user.getId()%></td>
+            <td><%=user.getFirstName()%></td>
+            <td><%=user.getLastName()%></td>
+            <td><%=user.getDateOfBirth()%></td>
+            <td><%=user.getAccessLevel()%></td>
+            <td><%=user.getEmail()%></td>
+            <td><%=user.getPasswordHash()%></td>
+            <td><%=user.getProfilePhoto()%></td>
+            <td><%=user.getStatusOnWall()%></td>
+            <td><%=user.getCity()%></td>
+        </tr>
+            <%
+        }
+    %>
+
+
+</section>
 
 </body>
 </html>
