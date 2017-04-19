6eb8800d73f78ef1400b7e0f9bec2d7f4c31f04d
VivaNova99 <veraivanova9999@gmail.com>
2017-04-20 01:01:04 +0300

    MyThemesController - sql-request, new method getMyThemes(), all relating .jsp files

--- src/main/java/dao/H2/H2WallMessageDao.java
+++ src/main/java/dao/H2/H2WallMessageDao.java
@@ -152,7 +152,7 @@ public class H2WallMessageDao implements WallMessageDao {
     @Override
     @SneakyThrows
     public Collection<WallMessage> getMyAnswers() {
-        List<WallMessage> AnswersWallMessages = new ArrayList<>();
+        List<WallMessage> myAnswersWallMessages = new ArrayList<>();
 
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
@@ -181,7 +181,7 @@ public class H2WallMessageDao implements WallMessageDao {
                      "WHERE wm.is_parent = FALSE AND wmparent.sender_user_id = 1")) {
             while (resultSet.next()){
                 SimpleDateFormat simpleFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                AnswersWallMessages.add(new WallMessage(
+                myAnswersWallMessages.add(new WallMessage(
                         resultSet.getInt("id"),
                         new User(
                                 resultSet.getString("first_name"),
@@ -203,7 +203,7 @@ public class H2WallMessageDao implements WallMessageDao {
                                 .orElseThrow(() -> new RuntimeException("нет такого статуса"))
                 ));
             }
-            return AnswersWallMessages;
+            return myAnswersWallMessages;
         }
     }
 
@@ -383,6 +383,65 @@ public class H2WallMessageDao implements WallMessageDao {
                 ));
             }
             return last10ForUserWallMessages;
+        }
+    }
+
+
+    @Override
+    @SneakyThrows
+    public Collection<WallMessage> getMyThemes() {
+        List<WallMessage> myThemesWallMessages = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT " +
+                     "wm.id, " +
+                     "wm.sender_user_id, " +
+                     "u.id, " +
+                     "u.first_name, " +
+                     "u.last_name, " +
+                     "wm.text as message_text, " +
+                     "wm.picture," +
+                     "wm.date_time, " +
+                     "wm.forum_theme_id, " +
+                     "wm.message_header, " +
+                     "ft.name," +
+                     "wm.is_parent, " +
+                     "wm.parent_message_id, " +
+                     "wmparent.id, " +
+                     "wmparent.sender_user_id," +
+                     "wmparent.text as parent_message_text, " +
+                     "wm.status_id " +
+                     "FROM WallMessage wm " +
+                     "JOIN User u ON wm.sender_user_id = u.id " +
+                     "JOIN ForumTheme ft ON wm.forum_theme_id = ft.id " +
+                     "JOIN WallMessage wmparent ON wm.parent_message_id = wmparent.id " +
+                     "WHERE wm.is_parent = TRUE AND wm.sender_user_id = 1")) {
+            while (resultSet.next()){
+                SimpleDateFormat simpleFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                myThemesWallMessages.add(new WallMessage(
+                        resultSet.getInt("id"),
+                        new User(
+                                resultSet.getString("first_name"),
+                                resultSet.getString("last_name")
+                        ),
+                        resultSet.getString("message_text"),
+                        resultSet.getString("picture"),
+                        simpleFormatter.parse(resultSet.getString("date_time")),
+                        new ForumTheme(
+                                resultSet.getString("name")
+                        ),
+                        resultSet.getString("message_header"),
+                        resultSet.getBoolean("is_parent"),
+                        new WallMessage(
+                                resultSet.getString("parent_message_text")
+                        ),
+                        MessageStatus.valueOf(
+                                resultSet.getInt("status_id") - 1)
+                                .orElseThrow(() -> new RuntimeException("нет такого статуса"))
+                ));
+            }
+            return myThemesWallMessages;
         }
     }
 
