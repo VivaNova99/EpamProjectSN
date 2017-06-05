60d0eb51eef441b08b20ce74445c55920e9d1331
VivaNova99 <veraivanova9999@gmail.com>
2017-06-05 21:08:39 +0300

    Editing User's info - 2

--- src/main/java/controllers/ChangeUserInfoController.java
+++ src/main/java/controllers/ChangeUserInfoController.java
@@ -95,7 +95,7 @@ public class ChangeUserInfoController extends HttpServlet {
         session.setAttribute("j_username", req.getParameter("email"));
         req.setAttribute("j_username", req.getParameter("email"));
 
-        req.setAttribute(USER_INFO_KEY, userDao.getUserTest(userId));
+//        req.setAttribute(USER_INFO_KEY, userDao.getUserTest(userId));
 
 
 //        req.getRequestDispatcher("/WEB-INF/test-pages/test-registration.jsp").forward(req, resp);
@@ -213,7 +213,7 @@ public class ChangeUserInfoController extends HttpServlet {
 
 //        req.setAttribute(WELCOME_KEY, s);
 
-        req.setAttribute(ALL_USERS_KEY, userDao.getAll());
+//        req.setAttribute(ALL_USERS_KEY, userDao.getAll());
 //        req.setAttribute(ALL_FORUM_THEMES_KEY, forumThemeDao.getAll());
 //        req.setAttribute(ALL_PHOTO_ALBUMS_KEY, photoAlbumDao.getAll());
 //        req.setAttribute(ALL_PHOTOS_KEY, photoDao.getAll());
